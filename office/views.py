d6e949163675cadb890a43fcbbb8476a755d361b
9730991252 <sunilkale101@gmail.com>
2025-01-06 17:08:27 +0530

    ok

--- office/views.py
+++ office/views.py
@@ -24,8 +24,9 @@ def remaining_account_holder_collection(request, account_type_id):
             account_holders = Account_holder.objects.filter(status=1)
             remaining_account_holders = []
             for account_holder in account_holders:
-                if not Transition.objects.filter(account_holder_id=account_holder.id, date=date.today(), account__account_type_id=account_type_id).exists():
-                    remaining_account_holders.append(account_holder)
+                if  Account.objects.filter(account_holder_id=account_holder.id, account_type_id=account_type_id).exists():
+                    if not Transition.objects.filter(account_holder_id=account_holder.id, date=date.today(), account__account_type_id=account_type_id).exists():
+                        remaining_account_holders.append(account_holder)
             context = {
                 'e': e,
                 'remaining_account_holders': remaining_account_holders,
